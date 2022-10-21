fb7c99b36704755f2dfb90f656fcfea64f121225
mr-abdellah <belkaid.abdulah@gmail.com>
2022-10-21 11:07:04 +0100

    Add responsive feature section

--- src/sections/FeatureSection/FeatureSection.jsx
+++ src/sections/FeatureSection/FeatureSection.jsx
@@ -3,20 +3,34 @@ import "./FeatureSection.css";
 import FindtrendImage from "../../assets/logo.png";
 import FeatureImg from "../../assets/img-feature.png";
 import FeatureImg1 from "../../assets/img-feature1.png";
+import FeatureImg2 from "../../assets/img-feature2.png";
+import { useState, useEffect } from "react";
 
 const FeatureSection = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWidth(window.innerWidth);
+    });
+  }, []);
+
   return (
     <div className="FeatureSection">
       <h1>Open new tabs is sh*t </h1>
       <div className="FeatureSection__illustration__container">
-        <img src={FeatureImg} alt="" />
+        <div className="featureimg__container1">
+          <img src={width > 768 ? FeatureImg2 : FeatureImg} alt="" />
+        </div>
         <div className="FeatureSection__illustration">
           <SocialMediaComponent
             img={FindtrendImage}
             title="Findtrend - Elon Musk"
           />
         </div>
-        <img src={FeatureImg1} alt="" />
+        <div className="featureimg__container2">
+          <img src={FeatureImg1} alt="" />
+        </div>
       </div>
       <p>
         A solution for your browser tabs and don't make your device get slower
